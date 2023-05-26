8d4ddf2711b2b7c584c9b6464b30329ee1762b96
charishmavemula <110437415+charishmavemula@users.noreply.github.com>
2023-05-26 21:23:04 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,27 +1,36 @@
-// NO PROBLEM HERE 
-
-module.exports = {
-    ensureAuth: (req, res, next)=>{
-        if(req.isAuthenticated()){
-            console.log("Authenticated");
-            return next();
-        }else{
-            console.log("Not Authenticated");
-            res.redirect('/');
-        }
-    },
-
-    ensureGuest: (req, res, next) =>{
-        if(req.isAuthenticated()){
-            console.log("Authenticated");
-           res.redirect('/dashboard');;
-        }else{
-            console.log("Not Authenticated");
-            return next();
-        }
-    },
-
-
-
-
-}
+//import { useState,useEffect,useContext,createContext } from "react"; 
+import { useState,useEffect,useContext,createContext } from "react"; 
+import axios from "axios"; 
+const AuthContext = createContext() 
+ 
+const AuthProvider =({children}) =>{ 
+        const [auth,setAuth] = useState({ 
+            user:null, 
+            token:"" 
+ 
+        }) 
+ 
+        axios.defaults.headers.common['Authorization'] = auth?.token 
+ 
+        useEffect(() =>{ 
+            const data = localStorage.getItem('auth') 
+            if(data){ 
+                const parseData = JSON.parse(data) 
+                setAuth({ 
+                    ...auth, 
+                    user:parseData.user, 
+                    token:parseData.token 
+                }) 
+            } 
+            //eslint-disable-next-line 
+        },[]) 
+        return( 
+        <AuthContext.Provider value={[auth,setAuth]}> 
+            {children} 
+         </AuthContext.Provider> 
+        ) 
+} 
+ 
+const useAuth = ()=> useContext(AuthContext) 
+ 
+export{useAuth,AuthProvider};
